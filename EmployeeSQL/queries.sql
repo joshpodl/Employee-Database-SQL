363321a747e02211a713458f8a14a96fa0456f03
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-04-04 17:23:47 -0400

    queries updated

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -5,14 +5,16 @@ employees.first_name,
 employees.sex,
 salaries.salary
 FROM salaries
-INNER JOIN employees ON employees.emp_no = salaries.emp_no;
+INNER JOIN employees ON employees.emp_no = salaries.emp_no
+ORDER BY emp_no;
 
 -- List first name, last name, and hire date for employees who were hired in 1986
 SELECT employees.first_name,
 employees.last_name,
 employees.hire_date
 FROM employees
-WHERE date_part('year', hire_date) = 1986;
+WHERE date_part('year', hire_date) = 1986
+ORDER BY emp_no;
 
 -- List the manager of each department with department number, department name, manager's employee number, last name, first name
 SELECT dept_manager.dept_no,
@@ -22,7 +24,8 @@ employees.last_name,
 employees.first_name
 FROM dept_manager
 LEFT JOIN departments ON dept_manager.dept_no = departments.dept_no
-LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no;
+LEFT JOIN employees ON dept_manager.emp_no = employees.emp_no
+ORDER BY dept_no;
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name
 SELECT dept_emp.emp_no,
@@ -31,14 +34,16 @@ employees.first_name,
 departments.dept_name
 FROM dept_emp
 LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
-LEFT JOIN departments ON departments.dept_no = dept_emp.dept_no;
+LEFT JOIN departments ON departments.dept_no = dept_emp.dept_no
+ORDER BY emp_no;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B"
 SELECT employees.first_name,
 employees.last_name,
 employees.sex
 FROM employees
-WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+ORDER BY emp_no;
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name
 SELECT employees.emp_no,
@@ -50,3 +55,4 @@ FROM employees
 LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
 WHERE departments.dept_name = 'Sales'
+ORDER BY emp_no;
