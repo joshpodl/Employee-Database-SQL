c599f27beaf45c75ecfa05609585b17c73979bcc
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-04-04 17:12:26 -0400

    more queries

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -31,4 +31,22 @@ employees.first_name,
 departments.dept_name
 FROM dept_emp
 LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
-LEFT JOIN departments ON departments.dept_no = dept_emp.dept_no
+LEFT JOIN departments ON departments.dept_no = dept_emp.dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B"
+SELECT employees.first_name,
+employees.last_name,
+employees.sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name
+SELECT employees.emp_no,
+employees.last_name,
+employees.first_name,
+dept_emp.dept_no,
+departments.dept_name
+FROM employees
+LEFT JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales'
