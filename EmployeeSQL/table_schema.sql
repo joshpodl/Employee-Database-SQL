e00323ba0b23568f385b4ad6c682d829f6fd5b0a
Josh Podl <josh@Joshs-MacBook-Pro-3.local>
2021-04-04 11:21:33 -0400

    table schema

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,69 @@
+-- Table schema
+
+-- Create titles table
+
+DROP TABLE titles
+
+CREATE TABLE titles (
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+)
+
+-- Create employees table
+
+DROP TABLE employees
+
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT Null,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- Create departments table
+
+DROP TABLE departments
+
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
+);
+
+-- Create dept_emp table
+
+DROP TABLE dept_emp
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- Create dept_manager table
+
+DROP TABLE dept_manager
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)	
+);
+
+-- Create salaries table
+
+DROP TABLE salaries
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
